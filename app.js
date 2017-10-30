da0bb97de917461eb783aa75bbc809312ebe6cb5
Viren Bhagat <virenb@gmail.com>
2017-10-29 22:26:53 -0400

    Updated fonts, clear and reset functions

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var humanPlayer;
 var aiPlayer;
 var gameRunning = false;
 var icons = ["O", "X"];
+var turns = [];
+var humanSquares = [];
+var aiSquares = [];
 
 function pickO() {
   document.getElementById("oButton").classList.add("btn-primary");
@@ -25,4 +28,27 @@ function pickX() {
   humanPlayer = "X";
   aiPlayer = "O";
 }
+
+function reset() {
+  document.getElementById("xButton").disabled = false;
+  document.getElementById("oButton").disabled = false;
+  document.getElementById("xButton").classList.remove("btn-danger");
+  document.getElementById("oButton").classList.remove("btn-primary");
+  humanPlayer = "";
+  aiPlayer = "";
+  clearSquares();
+}
+
+function clearSquares() {
+  var squares = document.getElementsByClassName("square");
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].innerText = "";
+  }
+}
+
+function playGame() {
+  reset();
+}
+
+
 
