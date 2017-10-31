ce633bc0060e7b84579f1848a30b61d6b4ab1fe5
Viren Bhagat <virenb@gmail.com>
2017-10-31 16:40:00 -0400

    Updated ai splicing, working on humanPlayer splicing and need to add winner logic

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-// Decide to play vs. self or AI
-// Pick X or 0
-// Able to place X or 0 in square
 // change turn after each placement
 // Make sure can only put one in a square and can't over write it
 // End game if a player has won before board is file
 // Display who's won
 // Clear board and play again
+// add color if win
 
 var humanPlayer;
 var aiPlayer;
 var board;
+var emptySquares = 9;
 var gameRunning = false;
-var icons = ["O", "X"];
-var turns = [];
+var turns = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 var humanSquares = [];
 var aiSquares = [];
+var currentPlayer = humanPlayer;
+var empty = "";
+var i;
+var winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+];
 
 function pickO() {
   document.getElementById("oButton").classList.add("btn-primary");
@@ -40,6 +51,9 @@ function reset() {
   humanPlayer = "";
   aiPlayer = "";
   clearSquares();
+  aiSquares = [];
+  humanSquares = [];
+  turns = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 }
 
 function clearSquares() {
@@ -49,8 +63,83 @@ function clearSquares() {
   }
 }
 
-function turn(square) {
+function humanTurn(square) {
   document.getElementById(square).innerText = humanPlayer;
+  i = turns.indexOf(square);
+  if (i > -1) {
+    turns.splice(i, 1);
+  }
+  humanSquares.push(square);
+  emptySquares--;
+  console.log('square is' + " " + square);
+  console.log('hu:' + '' + humanSquares);
+  console.log("turns:" + " " + turns);
+}
+
+function aiTurn(square) {
+  var aiMove = Math.floor(Math.random() * 8);
+  if (aiMove === 0) {
+    document.getElementById(aiMove).innerText = aiPlayer;
+    i = turns.indexOf(aiMove);
+    if (i > -1) {
+      turns.splice(i, 1);
+    }
+  } else if (aiMove === 1) {
+    document.getElementById(aiMove).innerText = aiPlayer;
+    i = turns.indexOf(aiMove);
+    if (i > -1) {
+      turns.splice(i, 1);
+    }
+  } else if (aiMove === 2) {
+    document.getElementById(aiMove).innerText = aiPlayer;
+    i = turns.indexOf(aiMove);
+    if (i > -1) {
+      turns.splice(i, 1);
+    }
+  } else if (aiMove === 3) {
+    document.getElementById(aiMove).innerText = aiPlayer;
+    i = turns.indexOf(aiMove);
+    if (i > -1) {
+      turns.splice(i, 1);
+    }
+  } else if (aiMove === 4) {
+    document.getElementById(aiMove).innerText = aiPlayer;
+    i = turns.indexOf(aiMove);
+    if (i > -1) {
+      turns.splice(i, 1);
+    }
+  } else if (aiMove === 5) {
+    document.getElementById(aiMove).innerText = aiPlayer;
+    i = turns.indexOf(aiMove);
+    if (i > -1) {
+      turns.splice(i, 1);
+    }
+  } else if (aiMove === 6) {
+    document.getElementById(aiMove).innerText = aiPlayer;
+    i = turns.indexOf(aiMove);
+    if (i > -1) {
+      turns.splice(i, 1);
+    }
+  } else if (aiMove === 7) {
+    document.getElementById(aiMove).innerText = aiPlayer;
+    i = turns.indexOf(aiMove);
+    if (i > -1) {
+      turns.splice(i, 1);
+    }
+  } else if (aiMove === 8) {
+    document.getElementById(aiMove).innerText = aiPlayer;
+    i = turns.indexOf(aiMove);
+    if (i > -1) {
+      turns.splice(i, 1);
+    }
+  } else {
+    console.log(hi);
+  }
+  aiSquares.push(aiMove);
+  emptySquares--;
+
+  console.log('ai:' + ' ' + aiSquares);
+  console.log("turns:" + " " + turns);
 }
 
 function playGame() {
@@ -61,8 +150,8 @@ function playGame() {
 }
 
 function turnClick(square) {
-  turn(square.target.id);
+  humanTurn(square.target.id);
+  aiTurn(square.target.id);
+
 }
-
-
 
