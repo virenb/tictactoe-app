dcb543d99b287de5b616f10ebdd72617e7532a62
Viren Bhagat <virenb@gmail.com>
2017-11-09 13:32:55 -0500

    Removed jQuery code, got click, reset working for human

--- app.js
+++ app.js
@@ -1,61 +1,14 @@
-// $(document).ready(function () {
-
-//   var human;
-//   var ai;
-//   var winner;
-//   var board = ["", "", "", "", "", "", "", "", ""];
-//   var currentPlayer;
-
-//   $("#oButton").click(function() {
-//     $(this).addClass("btn-primary");
-//     $("#xButton").attr("disabled", true);
-//     human = "O";
-//     ai = "X";
-//     //playGame();
-//   });
-
-//   $("#xButton").click(function () {
-//     $(this).addClass("btn-danger");
-//     $("#oButton").attr("disabled", true);
-//     human = "X";
-//     ai = "O";
-//     //playGame();
-//   });
-
-//   $("#play").click(function() {
-//     $("#oButton").attr("disabled", false);
-//     $("#xButton").attr("disabled", false);
-//     $("#oButton").removeClass("btn-primary");
-//     $("#xButton").removeClass("btn-danger");
-//     human = "";
-//     ai = "";
-//     board = ["", "", "", "", "", "", "", "", ""];
-//   });
-
-//   $('.square').click(function(event) {
-//     playerTurn(event);
-//   });
-
-//   // function playerTurn(event) {
-//   //   var position = $(event.target).attr('id');
-//   //   console.log(position);
-//   //   if (board[position] !== "") {
-//   //     console.log('hi');
-//   //   } else if (board[position] === "") {
-//   //     board[position] = human;
-//   //     $('.square[position]').text(human);
-//   // }}
-
-
-// });
-
 let human;
 let ai;
 let winner;
 let currentPlayer;
+let gameOver = false;
+let gameRunning = false;
 let oButton = document.getElementById('oButton');
 let xButton = document.getElementById('xButton');
 let playButton = document.getElementById('play');
+const squares = document.querySelectorAll('.square');
+let squareKeys = Object.keys(squares);
 
 const setO = () => {
   oButton.classList.add('btn-primary');
@@ -63,6 +16,7 @@ const setO = () => {
   oButton.disabled = false;
   human = 'O';
   ai = 'X';
+  play();
 };
 
 const setX = () => {
@@ -71,11 +25,41 @@ const setX = () => {
   oButton.disabled = true;
   human = 'X';
   ai = 'O';
+  play();
 };
 
-const squares = document.querySelectorAll('.square');
+const play = () => {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', turn, false);
+  }
+};
+
+const turn = (square) => {
+  humanTurn(square);
+};
+
+function remove(array, element) {
+  const index = array.indexOf(element);
+
+  if (index !== -1) {
+    array.splice(index, 1);
+  }
+}
+
+const humanTurn = (square) => {
+  let id = square.target.id;
+  if (squareKeys.includes(id)) {
+    document.getElementById(square.target.id).innerText = human;
+    remove(squareKeys, id);
+    console.log(squareKeys);
+  } else {
+    console.log('pick another number');
+  }
+
+};
 
 const clearSquares = () => {
+  let squares = document.getElementsByClassName('square');
   for (let i = 0; i < squares.length; i++) {
     squares[i].innerText = "";
   }
@@ -88,14 +72,24 @@ const resetPick = () => {
   oButton.disabled = false;
   xButton.classList.remove('btn-danger');
   oButton.classList.remove('btn-primary');
+  squareKeys = Object.keys(squares);
 };
 
 const playAgain = () => {
-  clearSquares();
   resetPick();
-}
+  clearSquares();
+  play();
+};
 
-oButton.addEventListener("click", setO);
-xButton.addEventListener("click", setX);
-playButton.addEventListener("click", playAgain);
-
+const checkWinner = (player) => {
+  var winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+};
